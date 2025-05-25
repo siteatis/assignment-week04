8f1c7716c94c51bd6c83ce3b34e849ecb592fb29
Jason Brunt <use@jsnb.uk>
2025-05-25 19:30:47 +0100

    chore: files omitted from previous commit

--- client/src/main.js
+++ client/src/main.js
@@ -5,28 +5,28 @@
 // create DOM ele to render data
 // - make sure you use DOM manipulation methods, avoid excessive .innerHTML
 //! during dev, fetch localhost; once deploying, swap localhost URLs with server deploy url
+//http://localhost:8080/staff --> dev
+//https://deployedwebsiteurl.onrender.com/staff --> production
 
-import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
+const log = console.log;
+const test = document.getElementById("test");
+test.addEventListener("submit", handleSubmitTest);
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector("#counter"));
+async function handleSubmitTest(ev) {
+  ev.preventDefault();
+  const testData = new FormData(test);
+  const msg = testData.get("testMessage"); // o = Object.fromEntries(testData);
+  let rsp = await fetch("http://localhost:8080/postTest", {
+    method: "POST", // This is where we set the POST HTTP verb
+    headers: {
+      "Content-Type": "application/json", // This tells the server we're sending stringified JSON data
+    },
+    body: JSON.stringify({ msg }),
+  });
+  log(rsp.status); // Expected: 200
+  // No neeed to await the response body, though I do worry as the server returns
+  // synchronously then we could be doing the GET before the data arrives in the DB
+  rsp = await fetch("http://localhost:8080/getTest");
+  const rspData = await rsp.json();
+  log(rspData.testmessage); // Expected: your own message returned to you
+}
